55d7c1856403f6ebd80328c27114702fd5940d1c
Thijs Busser <tbusser@gmail.com>
2020-03-11 15:07:38 +0100

    Update build to include ES5 version of library
    The build will output an ES5 compatible version of the package. The ES6
    version and type definitions have been moved to the esm/ folder.

--- scripts/build.sh
+++ scripts/build.sh
@@ -13,6 +13,9 @@ dir=${PACKAGE_OUTPUT_PATH:-"$root/dist"}
 echo "- Clean the destination folder"
 rm -rf "$dir"
 
+echo "- Transpile the TypeScript using Babel"
+env NODE_ENV='es5' babel src --source-root src --out-dir "$dir/" --ignore src/**/*.test.ts --extensions .ts --quiet
+
 echo "- Transpile the TypeScript using tsc"
 tsc -p tsconfig.emit.json
 
@@ -20,5 +23,9 @@ echo "- Transform and copy package.json"
 ./scripts/transform.js
 
 echo "- Copy README and License"
+
+echo "- Remove test files from dist folder"
+find "$dir" -type f -name "*.test.js" -delete
+
 cp LICENSE $dir
 cp README.md $dir
